85abbe008f54f5de85ef4e9d003b44b798965b3c
urimmmm <yulim0339@naver.com>
2023-10-17 23:33:00 +0900

    네이버 웹툰 팀장님과제
    Singleton과 Annotation 정리

--- Week3_Day1/Problem_2/Singleton.java
+++ Week3_Day1/Problem_2/Singleton.java
@@ -1,6 +1,13 @@
 package mission.Week3_Day1.Problem_2;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 // TODO: SingleTon Annotation을 만들어주세요. ( hint: Method, Runtime 에 동작 해야합니다. )
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Singleton {
     String name();
 }
--- Week3_Day1/Problem_2/SingletonContext.java
+++ Week3_Day1/Problem_2/SingletonContext.java
@@ -1,5 +1,7 @@
 package mission.Week3_Day1.Problem_2;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +15,34 @@ public class SingletonContext {
         SingletonContext.config = config;
     }
 
-    static synchronized Object getSingleton(String name) {
+    static synchronized Object getSingleton(String name) throws InvocationTargetException, IllegalAccessException {
         //TODO: 아래 구문 삭제 후, Map을 이용한 Singleton 생성 로직 구현 필요합니다.
-        return new Object();
+        //singletonObject를 Object 객체로 선언하고 이름을 가져옴
+        Object singletonObject = singletonObjectMap.get(name);
+        //만약 가져온 이름이 null값이라면 생성하기 전이기 떄문에 실행
+        if(singletonObject == null){
+            //singletonObject는 singletonMap에서 이름과 annotation으로 정의된 함수를 가져옴
+            singletonObject = singletonMap.get(name).invoke(config);
+            singletonObjectMap.put(name, singletonObject);
+        }
+        return singletonObject;
     }
     public static void executeMethodsBySingletonAnnotation(Object object) {
         // TODO - 1: Object 가 들어 오면 해당 클래스의 Method를 돌면서, @Singleton annotation을 발견합니다.
         // TODO - 2: 해당 Annotation의 name 과 method를 적당한 Map에 저장합니다.
+        Class<?> clazz = object.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for(Method method: methods){
+            Annotation[] annotations = method.getDeclaredAnnotations();
+
+            for(Annotation annotation: annotations){
+                if(annotation instanceof Singleton){
+                    Singleton singletonAnnotation = (Singleton) annotation;
+                    String name = singletonAnnotation.name();
+                    singletonMap.put(name, method);
+                }
+            }
+        }
     }
 }
